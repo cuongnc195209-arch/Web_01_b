9cec1c9b445c9fdafd8c7c2eddee02e93d567f76
cuongnc195209-arch <cuongnc195209@gmail.com>
2026-01-29 10:37:28 +0700

    u

---
+++ Web_06_a/src/java/Main/LogInServlet.java
@@ -0,0 +1,106 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package Main;
+
+import Model.UserDAO;
+import Model.UserDTO;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author Cuong
+ */
+public class LogInServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html;charset=UTF-8");
+        String url = "index.jsp";
+        HttpSession session = request.getSession();
+
+        if (session.getAttribute("user") == null) {
+            String txtuser = request.getParameter("txtuser");
+            String txtpass = request.getParameter("txtpass");
+
+            UserDAO udao = new UserDAO();
+            UserDTO user = udao.login(txtuser, txtpass);
+
+            System.out.println(user);
+            if (user != null) {
+                
+                if (user.isStatus()) {
+                    session.setAttribute("user", user);
+                    url = "a.jsp"; 
+                } else {
+                    url = "e403.jsp"; 
+                }
+            } else {
+                request.setAttribute("message", "Wrong pass or user");
+                url = "index.jsp";
+            }
+        } else {
+            url = "a.jsp";
+        }
+        request.getRequestDispatcher(url).forward(request, response);
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ Web_07_a/web/e403.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : newjsp
+    Created on : Jan 21, 2026, 10:14:27 PM
+    Author     : Cuong
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <p>403 do not allowed login</p>
+    </body>
+</html>
